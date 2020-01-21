51cb9182a722eeb4881040b96bb8c3cf5d054336
DanBurbach <dburbach1982@gmail.com>
2020-01-20 21:54:50 -0800

    basic template build for component tree

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import { Provider } from "react-redux";
+// import store from "./store";
+
+import * as ROUTES from "../constants/routes";
+
+import Main from "./Main";
+import Error404 from "./Error404";
+
+// import '../../assets/App';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <Provider store={store}> */}
+          <BrowserRouter>
+            <Switch>
+              <Route exact path={ROUTES.MAIN} component={Main} />
+              <Route component={Error404} />
+            </Switch>
+          </BrowserRouter>
+        {/* </Provider> */}
+      </div>
+    );
+  }
+}
+
+export default App;
