f5921cb8a9c12678cc27bb4b49efbe3f745f8368
DanBurbach <dburbach1982@gmail.com>
2020-01-24 13:12:02 -0800

    move the redux store from App.jsx into the main index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './assets/index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from 'redux-thunk';
 
+import rootReducer from "../src/reducers";
 import App from '../src/components/App.jsx';
-
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>,
 document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
