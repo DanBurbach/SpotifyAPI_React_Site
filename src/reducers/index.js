3cf7522b99aa3f568785f6086ab08b9520610a7c
DanBurbach <dburbach1982@gmail.com>
2020-01-24 13:10:48 -0800

    update reducer index to include modular reducer components

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,39 +1,14 @@
-import {FETCH_SONG_PENDING, FETCH_SONG_SUCCESS, FETCH_SONG_ERROR} from '../actions';
+import { combineReducers } from 'redux';
+import songReducer from './songReducer';
+import coreReducer from './songReducer';
 
-const initialState = {
-    pending: false,
-    song: [],
-    error: null
-}
+const rootReducer = combineReducers({
+    song: songReducer,
+    state: coreReducer
+});
 
-export function rootReducer(state = initialState, action){
-    return state;
-}
+export default rootReducer;
 
-export function SongReducer(state = initialState, action) {
-    switch(action.type) {
-        case FETCH_SONG_PENDING: 
-            return {
-                ...state,
-                pending: true
-            }
-        case FETCH_SONG_SUCCESS:
-            return {
-                ...state,
-                pending: false,
-                song: action.payload
-            }
-        case FETCH_SONG_ERROR:
-            return {
-                ...state,
-                pending: false,
-                error: action.error
-            }
-        default: 
-            return state;
-    }
-}
-
-export const getSong = state => state.song;
-export const getSongPending = state => state.pending;
-export const getSongError = state => state.error;
+// export const getSong = state => state.song;
+// export const getSongPending = state => state.pending;
+// export const getSongError = state => state.error;
