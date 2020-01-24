b439ebf33ac6cc2d9db69c990318af648a9d56cb
DanBurbach <dburbach1982@gmail.com>
2020-01-24 13:11:24 -0800

    export Pending, Success, and Error actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,20 +2,20 @@ export const FETCH_SONG_PENDING = 'FETCH_SONG_PENDING';
 export const FETCH_SONG_SUCCESS = 'FETCH_SONG_SUCCESS';
 export const FETCH_SONG_ERROR = 'FETCH_SONG_ERROR';
 
-function fetchSongPending() {
+export function fetchSongPending() {
     return {
         type: FETCH_SONG_PENDING
     }
 }
 
-function fetchSongSuccess(songs) {
+export function fetchSongSuccess(songs) {
     return {
         type: FETCH_SONG_SUCCESS,
         songs: songs
     }
 }
 
-function fetchSongError(error) {
+export function fetchSongError(error) {
     return {
         type: FETCH_SONG_ERROR,
         error: error
