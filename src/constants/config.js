fdff3127d9110f763ee2fb629491f22354ec555c
DanBurbach <dburbach1982@gmail.com>
2020-01-24 22:28:17 -0800

    using a tutorial add a temporary client_id for testing, will remove and replace later

--- src/constants/config.js
+++ src/constants/config.js
@@ -2,6 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "e04b7b361da94b4b8e57cc42a80d7196";
+
 export const redirectUri = "http://localhost:3000/redirect";
 export const scopes = [
     "user-top-read",
