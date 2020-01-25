0b8196d13ab1cf99ede57c4808eed6239637409e
DanBurbach <dburbach1982@gmail.com>
2020-01-24 22:25:10 -0800

    generate and import the MusicPlayer based off of the provided token upon login

---
+++ src/components/MusicPlayer/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const MusicPlayer = props => {
+    const backgroundStyles = {
+        backgroundImage: `url(${ props.item.album.iomages[0].url})`
+    };
+
+
+    const progressBarStyle = {
+        width: (props.progress_ms * 100 / props.item.duration_ms) + '%'
+    };
+
+return (
+    <div>
+        <div>
+            <div>
+                <img src={props.item.album.images[0].url}/>
+            </div>
+            <div>
+                <div>{props.item.name}</div>
+                <div>{props.item.artists[0].name}</div>
+                <div>{props.is_playing ? "Playing" : "Paused"}</div>
+                <div>
+                    <div className="progressBar" style={progressBarStyle}></div>
+                </div>
+            </div>
+            <div className="background" style={backgroundStyles} />{" "}
+        </div>
+    </div>
+    )
+}
+
+export default MusicPlayer;
