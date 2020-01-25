238b75d0965ce35de6c5149202a0cc011ddc1a19
DanBurbach <dburbach1982@gmail.com>
2020-01-24 22:27:01 -0800

    add list of ActionTypes used in the reducers, and reference

---
+++ src/constants/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "e04b7b361da94b4b8e57cc42a80d7196";
+export const redirectUri = "http://localhost:3000/redirect";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
