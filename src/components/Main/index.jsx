045c1da1d4c1c89aa32a765e6652fcd7932debb8
DanBurbach <dburbach1982@gmail.com>
2020-01-25 21:21:39 -0800

    build function for ajax connection to spotify API and add componentDidMount state set

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,59 +1,81 @@
 import React, { Component } from "react";
 import * as $ from 'jquery';
+import hash from './hash';
 import { authEndpoint, clientId, redirectUri, scopes } from "../../constants/config";
-import MusicPlayer from '../MusicPlayer';
+import Player from '../MusicPlayer';
+
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      articles: [
-        { title: "React Redux Tutorial for Beginners", id: 1 },
-        { title: "TypeScript tutorial for beginners", id: 2 }
-      ],
       token: null,
       item: {
         album: {
-          images: [{ url: ""}]
+          images: [{ url: "" }]
         },
         name: "",
-        artists: [{ name: ""}],
-        duration_ms:0,
+        artists: [{ name: "" }],
+        duration_ms: 0
       },
-      playing: "Paused",
+      is_playing: "Paused",
       progress_ms: 0
     };
+    this.getCurrentSongPlay = this.getCurrentSongPlay.bind(this);
   }
-  
+
+  componentDidMount() {
+    let _token = hash.access_token;
+
+    if (_token) {
+      this.setState({
+        token: _token
+      });
+      this.getCurrentSongPlay(_token);
+    }
+  }
+
+  getCurrentSongPlay(token) {
+    $.ajax({
+      url: "https://api.spotify.com/v1/me/player",
+      type: "GET",
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+      },
+      success: (data) => {
+        console.log("data", data);
+        this.setState({
+          item: data.item,
+          is_playing: data.is_playing,
+          progress_ms: data.progress_ms,
+        });
+      }
+    });
+  }
+
   render() {
-    const { articles } = this.state;
+
     return (
       <div>
-        <ul>
-          {articles.map(el => (
-            <li key={el.id}>{el.title}</li>
-          ))}
-        </ul>
-        <div>
+        <header className="App-header">
           {!this.state.token && (
-                <a
-                  className="btn btn--loginApp-link"
-                  href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-                    "%20"
-                  )}&response_type=token&show_dialog=true`}
-                >
-                  Login to Spotify
-                </a>
-              )}
-              {this.state.token && (
-                <MusicPlayer
-                  item={this.state.item}
-                  is_playing={this.state.is_playing}
-                  progress_ms={this.progress_ms}
-                />
-              )}
+            <a
+              className="btn btn--loginApp-link"
+              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                "%20"
+              )}&response_type=token&show_dialog=true`}
+            >
+              Login to Spotify
+            </a>
           )}
-        </div>
+          {this.state.token && (
+            <Player
+              item={this.state.item}
+              is_playing={this.state.is_playing}
+              progress_ms={this.progress_ms}
+            />
+          )}
+        </header>
       </div>
     );
   }
