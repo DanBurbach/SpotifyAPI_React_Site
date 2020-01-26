0aabdad1eeaf900c513ae0332eab825cd2e635f7
DanBurbach <dburbach1982@gmail.com>
2020-01-25 21:22:48 -0800

    bypass Redux store, and set aside use of actions and reducers until further testing is completed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from "redux";
-import thunkMiddleware from 'redux-thunk';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from "redux";
+// import thunkMiddleware from 'redux-thunk';
 
-import rootReducer from "../src/reducers";
-import App from '../src/components/App.jsx';
+// import rootReducer from "../src/reducers";
+import App from '../src/components/App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+// const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    // <Provider store={store}>
+        <App />,
+    // </Provider>,
 document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
