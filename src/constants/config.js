061648ffa1255c3d9d9da372a25de9dc03526519
DanBurbach <dburbach1982@gmail.com>
2020-04-16 21:31:07 -0700

    add back core config in constants folder for Spotify API reader

--- src/constants/config.js
+++ src/constants/config.js
@@ -3,7 +3,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "37ac6ac05bef4619b038491492e48bbe";
 
-export const redirectUri = "http://localhost:3000/redirect";
+export const redirectUri = "http://localhost:3000/";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
